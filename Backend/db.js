75dff294f5822582880c2888ce179c90771147fd
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-03-08 22:59:37 +0530

    backend done

--- Backend/db.js
+++ Backend/db.js
@@ -24,5 +24,5 @@ const todoSchema = mongoose.Schema({
 const todo = mongoose.model("todos", todoSchema);
 
 module.exports = {
-  todo: todo,
+  todo,
 };
--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,71 @@
-//write basic express boilerp;late code.
-//with express.json() middleware.
+// //write basic express boilerp;late code.
+// //with express.json() middleware.
 
+// const express = require("express");
+// const { createTodo, updateTodo } = require("./types");
+// const { todo } = require("./db");
+// const app = express();
+
+// app.use(express.json());
+
+// //body {
+// // title: string,
+// // description: string;
+// //}
+
+// app.post("/todo", function (req, res) {
+//   const createPayload = req.body;
+//   const parsedPayload = createTodo.safeParse(createPayload);
+//   if (!parsedPayload.success) {
+//     res.status(411).json({
+//       msg: "you sent wrong inputs",
+//     });
+//     return;
+//   }
+//else put in monogdb .
+//   todo.create({
+//     title: createPayload.title,
+//     description: createPayload.description,
+//     completed: false,
+//   });
+//   res.json({
+//     msg: "Todo created",
+//   });
+// });
+
+// app.get("/todos", async function (req, res) {
+//   const todos = await todo.fund({});
+
+//   res.json({
+//     todos,
+//   });
+//   //promise use async await
+// });
+
+// app.put("/completed", function (req, res) {
+//   const updatePayload = req.body;
+//   const parsedPayload = updateTodo.safeParse(updatePayload);
+//   if (!parsedPayload.success) {
+//     res.status(411).json({
+//       msg: "you sent the wrong inputs",
+//     });
+//     return;
+//   }
+
+//   await todo.update({
+//     _id: req.body.id
+//   }, {
+//     completed: true
+//   })
+//   res.json({
+//     msg:"Todo marked as completed"
+//   })
+// });
+
+// app.listen(3000);
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
@@ -16,25 +79,54 @@ app.post("/todo", function (req, res) {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
-    res.status(411).json({
-      msg: "you sent wrong inputs",
+    res.status(400).json({
+      msg: "You sent wrong inputs",
     });
     return;
   }
-  //else put in monogdb .
+  //else put in MongoDB.
+  todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "Todo created",
+  });
 });
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
 
-app.put("/completed", function (req, res) {
+  res.json({
+    todos,
+  });
+  //promise use async await
+});
+
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
-    res.status(411).json({
-      msg: "you sent the wrong inputs",
+    res.status(400).json({
+      msg: "You sent the wrong inputs",
     });
     return;
   }
+
+  await todo.updateOne(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
